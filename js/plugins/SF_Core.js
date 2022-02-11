0678fa639d39c7d0e6f091460ecdb54fdcc1617f
xian18 <44394537+xian18@users.noreply.github.com>
2022-02-11 23:24:54 +0800

    #publish ..

--- js/plugins/SF_Core.js
+++ js/plugins/SF_Core.js
@@ -441,7 +441,7 @@ var SF_Plugins = SF_Plugins || {};
             for (var i = 0; i < indexList.length; i++) {
                 this.unregister(indexList[i]);
             }
-        });
+        }).bind(this);
 
         for (var i = 0; i < indexList.length; i++) {
             this.setByClosureIndex(indexList[i], (function (i) {
