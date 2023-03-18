5ee9b9f4306e522595b9cca8c95ada15d2393c52
xian18 <44394537+xian18@users.noreply.github.com>
2023-03-19 01:03:20 +0800

    Modify Hit And Critical Formula
    根据 #16, 修改了命中率和暴击率的实现形式

--- js/plugins/SF_Objects.js
+++ js/plugins/SF_Objects.js
@@ -28,4 +28,36 @@ var SF_Plugins = SF_Plugins || {};
     Game_Action.prototype.applyCritical = function (damage) {
         return damage * 2.4;
     };
+
+    Game_Action.prototype.attackHit = function (target) {
+        return this.subject().hit;
+    }
+
+    SF_Objects.Game_Action_apply = Game_Action.prototype.apply;
+    Game_Action.prototype.apply = function (target) {
+        if ((this.isAttack() || this.isSkill())) {
+            var result = target.result();
+            this.subject().clearResult();
+            result.clear();
+            result.used = this.testApply(target);
+            result.missed = false;
+            result.evaded = (!result.missed && Math.random() >= (this.attackHit(target) - this.itemEva(target)));
+            result.physical = this.isPhysical();
+            result.drain = this.isDrain();
+            if (result.isHit()) {
+                if (this.item().damage.type > 0) {
+                    result.critical = (Math.random() < (this.subject().cri - target.cev));
+                    var value = this.makeDamageValue(target, result.critical);
+                    this.executeDamage(target, value);
+                }
+                this.item().effects.forEach(function (effect) {
+                    this.applyItemEffect(target, effect);
+                }, this);
+                this.applyItemUserEffect(target);
+            }
+        }
+        else {
+            SF_Objects.Game_Action_apply.applys(this, arguments);
+        }
+    };
 })();
